a83f9080580a7c9ec0b5b6f5b9c5de10806c806b
PabloMelo <pablomelodev@gmail.com>
2021-03-26 23:44:41 -0300

    feat: finished challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,16 +13,21 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository.createQueryBuilder();
-    // Complete usando query builder
+    return this.repository
+      .createQueryBuilder("game")
+      .where("game.title ILIKE :title", { title: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM games`);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository.createQueryBuilder();
-    // Complete usando query builder
+    return this.repository
+      .createQueryBuilder()
+      .relation("users")
+      .of(id)
+      .loadMany();
   }
 }
